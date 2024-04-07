0954c2896fd0a05cf5deed79ca77f683f898c0cf
Mycellf <jcouch210@gmail.com>
2024-04-07 10:45:29 -0800

    Added custom camera and changed test image

--- src/app.rs
+++ src/app.rs
@@ -1,15 +1,23 @@
 use macroquad::prelude::*;
 
-pub struct App {}
+pub struct App {
+    camera: Camera2D,
+}
 
 impl App {
     pub fn new() -> Self {
-        Self {}
+        let camera = Camera2D {
+            zoom: Vec2::splat(1.0 / 64.0),
+            ..Default::default()
+        };
+        Self { camera }
     }
 
     pub fn update(&mut self) {}
 
-    pub fn draw(&self) {
-        draw_text("Hello World", 10.0, 50.0, 40.0, WHITE);
+    pub fn draw(&mut self) {
+        self.camera.zoom.x = self.camera.zoom.y * screen_height() / screen_width();
+        set_camera(&self.camera);
+        draw_rectangle(0.0, 10.0, 5.0, 5.0, WHITE);
     }
 }
